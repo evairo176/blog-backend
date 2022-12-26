7b116711be9b2aea33ea3b48d2ee0a4be788fdae
Dicki Prasetya <dickiprasetya176@gmail.com>
2022-12-26 17:13:15 +0700

    feat: update

--- controller/users/UserController.js
+++ controller/users/UserController.js
@@ -300,7 +300,7 @@ const unBlockUserController = expressAsyncHandler(async (req, res) => {
 });
 
 //----------------------------------------------
-// Account verification =  send email
+// Account send email verification =
 //----------------------------------------------
 
 const verifUserController = expressAsyncHandler(async (req, res) => {
