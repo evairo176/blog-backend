810b6b75cd5fee1f2ca08885b8451d450caceb5a
Dicki Prasetya <dickiprasetya176@gmail.com>
2022-12-27 10:11:45 +0700

    feat: update

--- controller/users/UserController.js
+++ controller/users/UserController.js
@@ -300,7 +300,7 @@ const unBlockUserController = expressAsyncHandler(async (req, res) => {
 });
 
 //----------------------------------------------
-// send email verification account with mailtrap and sendgrid
+// send email verification account with mailtrap and sendgrid satu satu
 //----------------------------------------------
 
 const verifUserController = expressAsyncHandler(async (req, res) => {
