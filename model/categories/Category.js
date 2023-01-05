ddeac13362b9fdaec55b53bea854d3d966cd7d13
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-05 11:56:09 +0700

    feat: update

---
+++ model/categories/Category.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const categorySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const category = mongoose.model("Category", categorySchema);
+
+module.exports = category;
--- model/categorys/Category.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const categorySchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
