0224b21731c5cc3d32031299e10e6d2b38d6c175
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-05 13:40:33 +0700

    feat: update

--- controller/categories/CategoryController.js
+++ controller/categories/CategoryController.js
@@ -1,13 +1,31 @@
 const expressAsyncHandler = require("express-async-handler");
+const Category = require("../../model/categories/Category");
 
 //----------------------------------------------
 // create category
 //----------------------------------------------
 
 const createCategoryController = expressAsyncHandler(async (req, res) => {
-  res.json("abc");
+  try {
+    const category = await Category.create({
+      user: req.user._id,
+      title: req.body.title,
+    });
+    res.json(category);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//----------------------------------------------
+// fetch all category
+//----------------------------------------------
+
+const fetchAllCategoryController = expressAsyncHandler(async (req, res) => {
+  res.json("fetch all");
 });
 
 module.exports = {
   createCategoryController,
+  fetchAllCategoryController,
 };
--- route/categories/categoryRoutes.js
+++ route/categories/categoryRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const {
   createCategoryController,
+  fetchAllCategoryController,
 } = require("../../controller/categories/CategoryController");
 const authMiddleware = require("../../middleware/auth/authMiddleware");
 
 const categoryRoutes = express.Router();
 
 categoryRoutes.post("/", authMiddleware, createCategoryController);
+categoryRoutes.get("/", authMiddleware, fetchAllCategoryController);
 
 module.exports = {
   categoryRoutes,
