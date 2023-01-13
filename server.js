a62d38ede5596eaa3d411882a0ad5b142499941c
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-14 00:09:46 +0700

    feat: update

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.use("/api/categories", categoryRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-//server
+// server
 const port = process.env.PORT || 5000;
 app.listen(port, console.log("Express server listening on port: " + port));
