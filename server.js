6b75863b41d1ca56e0deb09149c8a2a2eb8448aa
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-02-22 15:35:31 +0700

    feat: update

--- server.js
+++ server.js
@@ -25,6 +25,17 @@ app.use(
     origin: "*",
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Origin",
+      "X-Requested-With",
+      "Accept",
+      "x-client-key",
+      "x-client-token",
+      "x-client-secret",
+      "Authorization",
+    ],
   })
 );
 
